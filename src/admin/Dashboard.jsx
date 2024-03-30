6b3d68604bf30d367b55904e6ae64923009a8e77
SHmohamed229 <SHmohamed229@gmail.com>
2024-03-30 21:41:27 +0200

    update Dashboard and Checkout

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -7,6 +7,7 @@ import useGetData from "../custom-hooks/useGetData";
 const Dashboard = () => {
   const {data : products} = useGetData('products');
   const {data : users} = useGetData('users');
+  const {data : orders} = useGetData('orders');
 
   return (
     <section>
@@ -21,7 +22,7 @@ const Dashboard = () => {
           <Col className="lg-3">
           <div className="order__box">
               <h5>Orders</h5>
-              <span>756</span>
+              <span>{orders.length}</span>
             </div>
           </Col>
           <Col className="lg-3">
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,116 +1,3 @@
-// import React ,{useState} from "react";
-// import { Container , Row ,Col , Form , FormGroup } from "reactstrap";
-// import Helmet from '../components/Helmet/Helmet'
-// import CommonSection from '../components/UI/CommonSection'
-// import { useSelector } from "react-redux";
-
-// import {toast} from 'react-toastify';
-// import { db ,storage } from "../firebase.config";
-// import { ref , uploadBytesResumable , getDownloadURL } from "firebase/storage";
-// import { collection , addDoc } from "firebase/firestore";
-// import { useNavigate } from "react-router-dom";
-
-// import '../styles/checkout.css';
-
-// const Checkout = () => {
-//     const totalQty = useSelector(state =>state.cart.totalQuantity);
-//     const totalAmount = useSelector(state =>state.cart.totalAmount);
-
-//     const [formData, setFormData] = useState({
-//         name: '',
-//         email: '',
-//         phoneNumber: '',
-//         streetAddress: '',
-//         city: '',
-//         postalCode: '',
-//         country: ''
-//     });
-
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target;
-//         setFormData(prevState => ({
-//             ...prevState,
-//             [name]: value
-//         }));
-//     };
-
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//             // إرسال بيانات النموذج إلى Firebase
-//             const docRef = await addDoc(collection(db, 'orders'), formData);
-//             // إظهار رسالة تأكيد مع رقم الطلب
-//             toast.success(`Order placed successfully. Order ID: ${docRef.id}`);
-//             // تفريغ النموذج
-//             setFormData({
-//                 name: '',
-//                 email: '',
-//                 phoneNumber: '',
-//                 streetAddress: '',
-//                 city: '',
-//                 postalCode: '',
-//                 country: ''
-//             });
-//         } catch (error) {
-//             console.error('Error adding document: ', error);
-//             toast.error('Failed to place order. Please try again.');
-//         }
-//     };
-
-//     return <Helmet title="Checkout">
-//         <CommonSection  title="Checkout"/>
-//         <section>
-//             <Container>
-//                 <Row>
-//                     <Col lg='8'>
-//                         <h6 className="mb-4 fw-bold">Billing Information</h6>
-//                         <Form   className="billing__form"  onSubmit={handleSubmit}>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="Enter Your Name" name="name" value={formData.name} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="email" placeholder="Enter Your Email" name="email" value={formData.email} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="Phone number" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="Street address" name="streetAddress" value={formData.streetAddress} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="City"  name="city" value={formData.city} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="Postal code" name="postalCode" value={formData.postalCode} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                             <FormGroup className="form__group">
-//                                 <input type="text" placeholder="Country" name="country" value={formData.country} onChange={handleInputChange}/>
-//                             </FormGroup>
-//                         </Form>
-//                     </Col>
-
-//                     <Col lg='4'>
-//                         <div className="checkout__cart">
-//                             <h6>Total Qty : <span>{totalQty} items</span></h6>
-//                             <h6>Subtotal : <span>${totalAmount}</span></h6>
-//                             <h6><span>Shipping : <br />Free Shipping </span> <span>$0</span></h6>
-//                             <h4>Total Cost : <span>${totalAmount}</span></h4>
-//                             <button onClick={handleSubmit} className="bay__btn auth__btn w-100">Place an order</button>
-//                         </div>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         </section>
-//     </Helmet>
-// }
-
-// export default Checkout;
-
-
-
-
-
 
 import React, { useState } from "react";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
@@ -126,6 +13,7 @@ import '../styles/checkout.css';
 const Checkout = () => {
     const totalQty = useSelector(state => state.cart.totalQuantity);
     const totalAmount = useSelector(state => state.cart.totalAmount);
+    const productName = useSelector(state => state.cart.productName);
 
     const [formData, setFormData] = useState({
         name: '',
@@ -148,11 +36,22 @@ const Checkout = () => {
     const handleSubmit = async (e) => {
 
         e.preventDefault();
+
+        // التحقق من اكتمال بيانات الفورم
+    const formFilled = Object.values(formData).every(value => value !== '');
+    if (!formFilled || totalQty === 0 || totalAmount === 0) {
+        // إذا لم تكن بيانات الفورم مكتملة، عرض رسالة تنبيه
+        toast.error('Please fill in all the fields before placing the order.');
+        return; // إيقاف تنفيذ الدالة إذا لم تكن بيانات الفورم مكتملة
+    }
+
         try {
             // توسيع بيانات الفورم لتضمين تفاصيل الطلب أيضًا
             const orderData = {
                 ...formData,
-                items: [], // قائمة بالمشتريات التي اختارها المستخدم، يمكن ملء هذا الجزء بناءً على بيانات السلة في Redux
+                
+                    items: Array.isArray(productName) ? [...productName] : [], 
+                 // قائمة بالمشتريات التي اختارها المستخدم، يمكن ملء هذا الجزء بناءً على بيانات السلة في Redux
                 totalQuantity: totalQty,
                 totalAmount: totalAmount
             };
@@ -171,9 +70,12 @@ const Checkout = () => {
                 country: ''
             });
 
-            window.location.reload();
+            // window.location.reload();
                         // إظهار رسالة تأكيد مع رقم الطلب
                         toast.success(`Order placed successfully. Order ID: ${docRef.id}`);
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 3000);
         } catch (error) {
             console.error('Error adding document: ', error);
             toast.error('Failed to place order. Please try again.');
@@ -190,25 +92,25 @@ const Checkout = () => {
                             <h6 className="mb-4 fw-bold">Billing Information</h6>
                             <Form className="billing__form" onSubmit={handleSubmit}>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="Enter Your Name" name="name" value={formData.name} onChange={handleInputChange} />
+                                    <input type="text" placeholder="Enter Your Name" name="name" value={formData.name} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="email" placeholder="Enter Your Email" name="email" value={formData.email} onChange={handleInputChange} />
+                                    <input type="email" placeholder="Enter Your Email" name="email" value={formData.email} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="Phone number" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
+                                    <input type="text" placeholder="Phone number" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="Street address" name="streetAddress" value={formData.streetAddress} onChange={handleInputChange} />
+                                    <input type="text" placeholder="Street address" name="streetAddress" value={formData.streetAddress} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="City" name="city" value={formData.city} onChange={handleInputChange} />
+                                    <input type="text" placeholder="City" name="city" value={formData.city} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="Postal code" name="postalCode" value={formData.postalCode} onChange={handleInputChange} />
+                                    <input type="text" placeholder="Postal code" name="postalCode" value={formData.postalCode} onChange={handleInputChange} required/>
                                 </FormGroup>
                                 <FormGroup className="form__group">
-                                    <input type="text" placeholder="Country" name="country" value={formData.country} onChange={handleInputChange} />
+                                    <input type="text" placeholder="Country" name="country" value={formData.country} onChange={handleInputChange} required/>
                                 </FormGroup>
                             </Form>
                         </Col>
